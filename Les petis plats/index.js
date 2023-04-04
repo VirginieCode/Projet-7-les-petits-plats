9f26d3156590c41d17147ffea094704d8272817c
VirginieCode <dmy.virginie@gmail.com>
2023-04-04 16:01:42 +0100

    Modification style search bar et implémentation de l'algorithme pour afficher recettes à partir de leur nom via a la search bar

--- Les petis plats/index.js
+++ Les petis plats/index.js
@@ -1,4 +1,4 @@
-// Fonction toggle pour le dropdown
+// Fonction ouvrir et fermer le dropdown
 
 const dropdowns = document.querySelectorAll('.dropdown');
 
@@ -11,6 +11,7 @@ dropdowns.forEach(function (dropdown) {
   });
 });
 
+
 document.addEventListener('click', function (event) {
   dropdowns.forEach(function (dropdown) {
     const dropdownMenu = dropdown.querySelector('.dropdown-menu');
@@ -21,65 +22,93 @@ document.addEventListener('click', function (event) {
   });
 });
 
-// fetch pour récuprer les recettes 
+//Lecture des termes entrés dans la barre de recherche
+
+const searchBar = document.querySelector('#searchBar');
+
+searchBar.addEventListener('input', (event) => {
+  const searchInput = event.target;
+  const searchTerm = searchInput.value.toLowerCase().trim();
+  filterRecipes(searchTerm);
+});
+
+//Filtrage des recettes
+
+let allRecipes = [];
+
+function filterRecipes(searchTerm) {
+  const filteredRecipes = allRecipes.filter(recipe => {
+    const recipeName = recipe.name.toLowerCase();
+    return recipeName.includes(searchTerm);
+  });
+  displayRecipes(filteredRecipes);
+}
+
+// Création des card des recettes
+
+function displayRecipes(recipes) {
+  const section = document.querySelector('.recipeDisplay');
+  section.innerHTML = ''; 
+
+  for (let i = 0; i < recipes.length; i++) {
+    const card = document.createElement('div');
+    const image = document.createElement('div');
+    const infos = document.createElement('div');
+    const recipeNameAndTime = document.createElement('div');
+    const recipeName = document.createElement('p');
+    const recipeTime = document.createElement('p');
+    const ingredientsAndDescription = document.createElement('div');
+    const recipeDescription = document.createElement('div');
+    const recipeIngredients = document.createElement('div');
+
+    card.classList.add('recipeCard');
+    image.classList.add('recipeCardImage');
+    infos.classList.add('recipeCardInfos');
+    recipeDescription.classList.add('recipeDescription');
+    recipeIngredients.classList.add('recipeIngredients');
+    ingredientsAndDescription.classList.add('ingredientsAndDescription')
+    recipeName.classList.add('recipeName');
+    recipeNameAndTime.classList.add('recipeNameAndTime');
+
+    recipeName.textContent = `${recipes[i].name}`;
+    recipeTime.textContent = `${recipes[i].time}`;
+    recipeDescription.textContent = `${recipes[i].description}`;
+
+    recipeNameAndTime.appendChild(recipeName);
+    recipeNameAndTime.appendChild(recipeTime);
+    infos.appendChild(recipeNameAndTime);
+
+    recipes[i].ingredients.forEach(ingredient => {
+      const ingredientName = ingredient.ingredient;
+    
+      const ingredientItem = document.createElement('li');
+      ingredientItem.textContent = `${ingredient.ingredient}: ${ingredient.quantity} ${ingredient.unit || ''}`;
+      recipeIngredients.appendChild(ingredientItem);
+      infos.appendChild(ingredientsAndDescription);
+    });
+    
+
+    section.appendChild(card);
+
+    card.appendChild(image);
+
+    card.appendChild(infos);
+
+    ingredientsAndDescription.appendChild(recipeIngredients);
+    ingredientsAndDescription.appendChild(recipeDescription);
+
+
+  }
+}
+
+//Fetch pour récuprer le data 
 
 fetch('data/recipes.json')
   .then(response => response.json())
   .then(data => {
-    // `data` contient les données du tableau JSON
-    console.log(data);
-    const section = document.querySelector('.recipeDisplay');
-    for (let i = 0; i < data.length; i++) {
-
-      const card = document.createElement('div');
-      const image = document.createElement('div');
-      const infos = document.createElement('div');
-      const recipeNameAndTime = document.createElement('div');
-      const recipeName = document.createElement('p');
-      const recipeTime = document.createElement('p');
-      const ingredientsAndDescription = document.createElement('div');
-      const recipeDescription = document.createElement('div');
-      const recipeIngredients = document.createElement('div');
-
-      card.classList.add('recipeCard');
-      image.classList.add('recipeCardImage');
-      infos.classList.add('recipeCardInfos');
-      recipeDescription.classList.add('recipeDescription');
-      recipeIngredients.classList.add('recipeIngredients');
-      ingredientsAndDescription.classList.add('ingredientsAndDescription')
-      recipeName.classList.add('recipeName');
-      recipeNameAndTime.classList.add('recipeNameAndTime')
-
-
-
-
-      recipeName.textContent = `${data[i].name}`;
-      recipeTime.textContent = `${data[i].time}`;
-      recipeDescription.textContent = `${data[i].description}`;
-
-      recipeNameAndTime.appendChild(recipeName);
-      recipeNameAndTime.appendChild(recipeTime);
-      infos.appendChild(recipeNameAndTime);
-
-      data[i].ingredients.forEach(ingredient => {
-        const ingredientItem = document.createElement('li');
-        ingredientItem.textContent = `${ingredient.ingredient}: ${ingredient.quantity} ${ingredient.unit || ''}`;
-        recipeIngredients.appendChild(ingredientItem);
-        infos.appendChild(ingredientsAndDescription);
-
-      });
-      section.appendChild(card);
-
-      card.appendChild(image);
-
-      card.appendChild(infos);
-
-      ingredientsAndDescription.appendChild(recipeIngredients);
-      ingredientsAndDescription.appendChild(recipeDescription);
-
-
-
-    }
+    allRecipes = data; 
+    displayRecipes(data);
   })
   .catch(error => console.error(error));
+
 
